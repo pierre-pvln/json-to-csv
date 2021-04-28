163d14d4de35390709d0c7af4b7d356d45471c0c
IPHPY38PIP <info@ipheion.eu>
2021-04-28 15:20:49 +0200

    updated

--- run_local/set_project/select_iph_Born_202104_test.cmd
+++
@@ -1,1 +0,0 @@
-copy ..\..\code\settings\Born\projectsettings_iph_202104_test.json ..\..\code\app\projectsettings.json
---
+++ set_project/select_iph_Born_202104_test.cmd
@@ -0,0 +1,1 @@
+copy ..\code\settings\Born\projectsettings_iph_202104_test.json ..\code\app\projectsettings.json
