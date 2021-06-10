75fcc3409823963f61103e8060019166ff638a2a
IPHPY38PIP <info@ipheion.eu>
2021-06-10 11:27:35 +0200

    updated

---
+++ copy_shipsdata.cmd
@@ -0,0 +1,5 @@
+copy "C:\Users\developer\OneDrive\@pvln_coding_PVE\myShipsData\billing_info.xlsx" .\code\input\
+copy "C:\Users\developer\OneDrive\@pvln_coding_PVE\myShipsData\ship_info.xlsx" .\code\input\
+copy "C:\Users\developer\OneDrive\@pvln_coding_PVE\myShipsData\BRON billing_info.xlsx - Snelkoppeling.lnk" .\code\input\
+copy "C:\Users\developer\OneDrive\@pvln_coding_PVE\myShipsData\BRON ship_info.xlsx - Snelkoppeling.lnk" .\code\input\
+ 
