4f1dfa21929ae794d5e43ddfada22f3c1b2a015c
Pierre Veelen (pvln) <pierre@pvln.nl>
2025-04-06 18:50:51 +0200

    updated

--- copy_shipsdata.cmd
+++ copy_shipsdata.cmd
@@ -1,5 +1,5 @@
-copy "C:\Users\developer\OneDrive\@pvln_coding_PVE\myShipsData\billing_info.xlsx" .\code\input\
-copy "C:\Users\developer\OneDrive\@pvln_coding_PVE\myShipsData\ship_info.xlsx" .\code\input\
-copy "C:\Users\developer\OneDrive\@pvln_coding_PVE\myShipsData\BRON billing_info.xlsx - Snelkoppeling.lnk" .\code\input\
-copy "C:\Users\developer\OneDrive\@pvln_coding_PVE\myShipsData\BRON ship_info.xlsx - Snelkoppeling.lnk" .\code\input\
+copy "C:\Users\developer\OneDrive\@dev_coding\myShipsData\billing_info.xlsx" .\code\input\
+copy "C:\Users\developer\OneDrive\@dev_coding\myShipsData\ship_info.xlsx" .\code\input\
+copy "C:\Users\developer\OneDrive\@dev_coding\myShipsData\BRON billing_info.xlsx - Snelkoppeling.lnk" .\code\input\
+copy "C:\Users\developer\OneDrive\@dev_coding\myShipsData\BRON ship_info.xlsx - Snelkoppeling.lnk" .\code\input\
  
